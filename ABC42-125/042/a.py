b6986a1b1327ad0ddcf60c9d26fbf632d8575c3f
isseii10 <t1l.g2.o11g@gmail.com>
2022-12-19 03:05:41 +0900

    fix dir

--- ABC42-125/042/a.py
+++
@@ -1,6 +0,0 @@
-abc = list(map(int, input().split()))
-gogonana = [[5, 5, 7], [5, 7, 5], [7, 5, 5]]
-if abc in gogonana:
-    print("YES")
-else:
-    print("NO")
--- ABC42-125/155/d2.py
+++
@@ -1,5 +0,0 @@
-from bisect import bisect_right
-
-a = [1, 2, 3, 3, 4, 5, 5]
-i = bisect_right(a, 3)
-print(i)
